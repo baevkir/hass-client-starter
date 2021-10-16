1a6b3e1e842094013c4ec0ba62a216b3b00cced1
kirillbaev <kbaiev@provectus.com>
2021-10-16 18:00:13 +0300

    add post method for StateClient

--- src/main/java/com/kb/hass/client/model/State.java
+++ src/main/java/com/kb/hass/client/model/State.java
@@ -1,11 +1,17 @@
 package com.kb.hass.client.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class State {
     @JsonProperty("entity_id")
     private String entityId;
